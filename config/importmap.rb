302f2ba112dcc16715b5c707bd5a3fd4e0910ac5
aejohnmartin <132005590+aejohnmartin@users.noreply.github.com>
2026-09-12 18:28:15 -0400

    Install DaisyUI, Tailwind Forms, and Tailwind Typography

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,12 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "trix"
 pin "@rails/actiontext", to: "actiontext.esm.js"
+pin "@tailwindcss/typography", to: "@tailwindcss--typography.js" # @0.5.20
+pin "cssesc" # @3.0.0
+pin "postcss-selector-parser" # @6.0.10
+pin "tailwindcss/colors", to: "tailwindcss--colors.js" # @4.3.3
+pin "tailwindcss/plugin", to: "tailwindcss--plugin.js" # @4.3.3
+pin "util-deprecate" # @1.0.2
+pin "@tailwindcss/forms", to: "@tailwindcss--forms.js" # @0.5.11
+pin "mini-svg-data-uri" # @1.4.4
+pin "tailwindcss/defaultTheme", to: "tailwindcss--defaultTheme.js" # @4.3.3
